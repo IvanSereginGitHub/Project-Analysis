ea9ebe563b898b2626d63f724dbe97058b808b41
Ivan Seregin <vanIvan4.work@gmail.com>
2024-05-27 14:50:21 +0500

    Fix keys

--- Assets/KeysShortcut.cs
+++ Assets/KeysShortcut.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class KeysShortcut : MonoBehaviour
 {
     public KeyCode keyCode;
     public UnityEvent ev = new UnityEvent();
+    EventSystem system;
+
+    void Start()
+    {
+        system = FindObjectOfType<EventSystem>();
+    }
     void Update()
     {
-        if(Input.GetKeyUp(keyCode)) {
+        if (Input.GetKeyUp(keyCode) && system.currentSelectedGameObject == null)
+        {
             ev.Invoke();
         }
     }
